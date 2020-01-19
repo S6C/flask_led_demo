ce2136dad660fbae96e682218ea4d7bbd1e2e1c0
Craig Chambers <cchambers@salisbury6c.ac.uk>
2020-01-19 14:30:02 +0000

    Added control of SenseHat

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import json
-
+from sense_hat import SenseHat
+sense = SenseHat()
 
 def ledControl(status):
     if status == True:
@@ -8,6 +9,39 @@ def ledControl(status):
     else:
         print("LED OFF")
 
+
+def senseControl(status):
+    X = [255, 255, 255]  # On
+    O = [0, 0, 0]  # Off
+
+    off = [
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O
+    ]
+
+    on = [
+        X, X, X, X, X, X, X, X,
+        X, X, X, X, X, X, X, X,
+        X, X, X, X, X, X, X, X,
+        X, X, X, X, X, X, X, X,
+        X, X, X, X, X, X, X, X,
+        X, X, X, X, X, X, X, X,
+        X, X, X, X, X, X, X, X,
+        X, X, X, X, X, X, X, X
+    ]
+
+    if status == True:
+        sense.set_pixels(on)
+    if status == False:
+        sense.set_pixels(off)
+    else:
+        print('Error setting pixels')  
 
 app = Flask(__name__)
 
@@ -28,8 +62,9 @@ def ledStatus():
         data = request.get_json()
         led = data['led']
         ledControl(led)
+        senseControl(led)
         return json.dumps(data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
